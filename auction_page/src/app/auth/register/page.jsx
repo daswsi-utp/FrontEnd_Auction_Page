39f2513bf8b9702a9ca7129b9ba6f4b9f51166d1
huahuaccapa <huahuaccapa1119@gmail.com>
2025-04-18 12:49:51 -0500

    modificaciones

--- auction_page/src/app/auth/register/page.jsx
+++ auction_page/src/app/auth/register/page.jsx
@@ -1,105 +1,23 @@
-'use client';
-
-import { useState } from 'react';
-import styles from '../register/register.module.css'; 
-
-const RegisterPage = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      alert('Las contraseñas no coinciden');
-      return;
-    }
-
-    const userData = {
-      name: username,
-      email,
-      password,
-      role: 'Comprador/Vendedor',
-      joined: new Date().toISOString().split('T')[0],
-      avatar: '/path/to/user-icon.png',
-      auctionsCreated: 0,
-      bidsPlaced: 0,
-    };
-
-    localStorage.setItem('user', JSON.stringify(userData));
-    window.location.href = '/profile';
-  };
-
-  return (
-    <div className={styles.loginContainer}>
-      <div className={styles.loginBox}>
-        <div className={styles.icon}>
-          <img src="/path/to/user-icon.png" alt="User Icon" />
+function UserRegister() {
+    return (
+        <div className="h-[calc(100hv-7rem)] flex justify-center items-center">
+            <form action="">
+                <img src="https://ayudawp.com/wp-content/uploads/2014/02/usuario-registrado.jpg" className="rounded-full w-50"/>
+                <h1 className="text-slate-200 font-bold text-4xl mb-4">REGISTER</h1>
+                <label htmlFor="username" className="text-slate-500 mb-2 block text-sm">Username:</label>
+                <input type="text" className="p-3 rounded block mb-2 bg-slate-900 text-slate-300"/>
+                <label htmlFor="email" className="text-slate-500 mb-2 block text-sm">Email:</label>
+                <input type="email" className="p-3 rounded block mb-2 bg-slate-900 text-slate-300"/>
+                <label htmlFor="password" className="text-slate-500 mb-2 block text-sm">Password:</label>
+                <input type="password" className="p-3 rounded block mb-2 bg-slate-900 text-slate-300"/>
+                <label htmlFor="confirmPassword" className="text-slate-500 mb-2 block text-sm">Confirm Password:</label>
+                <input type="confirmPassword" className="p-3 rounded block mb-2 bg-slate-900 text-slate-300"/>
+                <button className="w-full bg-blue-500 text-white p-3 rounded-lg mt-3">
+                    REGISTRAR
+                </button>
+            </form>
         </div>
-        <h1 className={styles.title}>Registro de Usuario</h1>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.inputGroup}>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              placeholder="Enter your username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
+    )
+}
 
-          <div className={styles.inputGroup}>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className={styles.inputGroup}>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className={styles.inputGroup}>
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              placeholder="Confirm your password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className={styles.links}>
-            <a href="/auth/login" className={styles.forgotPassword}>
-              ¿Ya tienes una cuenta? Inicia sesión
-            </a>
-          </div>
-
-          <button type="submit" className={styles.loginButton}>
-            Registrarse
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
-
-export default RegisterPage;
+export default UserRegister
