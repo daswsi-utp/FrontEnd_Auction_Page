1c1b5f0394a23dc3d87cf5cc2dce9ac188de4d81
LeydySenati <1463407@senati.pe>
2025-04-18 18:06:00 -0500

    Update
    Update

--- auction_page/src/components/NavBar.jsx
+++ auction_page/src/components/NavBar.jsx
@@ -25,7 +25,7 @@ export default function NavBar() {
         <nav className="flex flex-col gap-4 p-4">
           <Link href="/" className="hover:text-blue-400 transition-colors">🏠 {isOpen && 'Inicio'}</Link>
           <Link href="/Auth/Register" className="hover:text-blue-400 transition-colors">📝 {isOpen && 'Registro'}</Link>
-          {/* AQUI PONDRE LOS OTROS LINKS */}
+          {/* AQUI PONDRE LOS OTROS LINKS g*/}
         </nav>
       </div>
     </div>
